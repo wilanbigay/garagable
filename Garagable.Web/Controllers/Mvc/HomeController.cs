5169e33983d6bb7a1034569092f8df3f9543d1c8
Wilan Bigay <wilan.bigay@gmail.com>
2012-12-17 22:23:25 +1100

    Move HomeController to Mvcfolder

---
+++ Garagable.Web/Controllers/Mvc/HomeController.cs
@@ -0,0 +1,9 @@
+﻿using System.Web.Mvc;
+
+namespace Garagable.Web.Controllers.Mvc {
+    public class HomeController : Controller {
+        public ActionResult Index() {
+            return View();
+        }
+    }
+}
