dcbd10e6d49247eaba2f7703549d1148c46a6b72
Wilan Bigay <wilan.bigay@gmail.com>
2013-01-13 09:54:58 +1100

    Migrations InitSchema

--- Garagable.Data/Migrations/Configuration.cs
+++ Garagable.Data/Migrations/Configuration.cs
@@ -9,7 +9,7 @@ namespace Garagable.Data.Migrations
     {
         public Configuration()
         {
-            AutomaticMigrationsEnabled = false;
+            AutomaticMigrationsEnabled = true;
         }
 
         protected override void Seed(Garagable.Data.GaragableContext context)
